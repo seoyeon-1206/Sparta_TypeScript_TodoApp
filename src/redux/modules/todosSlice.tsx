0d4b820cb9e2971f1f611a3ea9247fac4edadcc1
seoyeon-1206 <ssy_1206@naver.com>
2024-03-07 14:49:13 +0900

    [feat] store, reducer 작성

---
+++ src/redux/modules/todosSlice.tsx
@@ -0,0 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit";
+import Todo from "../../models/todo";
+
+interface TodosState {
+  todos: Todo[];
+}
+
+const initialState: TodosState = {
+  todos: [],
+};
+
+const todosSlice = createSlice({
+  name: "todos",
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      state.todos.push(action.payload);
+    },
+    deleteTodo: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+    },
+    switchTodo: (state, action) => {
+      const todo = state.todos.find((todo) => todo.id === action.payload);
+      if (todo) {
+        todo.isDone = !todo.isDone;
+      }
+    },
+  },
+});
+
+export const { addTodo, deleteTodo, switchTodo } = todosSlice.actions;
+export default todosSlice.reducer;

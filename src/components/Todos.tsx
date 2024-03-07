119b0ce6a52eaaf12e4759924bcf37fd38db40f8
seoyeon-1206 <ssy_1206@naver.com>
2024-03-07 12:22:31 +0900

    [feat] porgress상태 변경 기능 구현

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -11,25 +11,65 @@ const Todos: React.FC<{
     props.setTodos(updateTodos);
   };
 
+  const handleProgressUpdate = (id: string) => {
+    const updateTodos: Todo[] = props.todos.map((todo) => {
+      return todo.id === id ? { ...todo, isDone: !todo.isDone } : todo;
+    });
+    props.setTodos(updateTodos);
+  };
+
   return (
     <Container>
-      {props.todos.map((todo) => (
-        <TodoListWrapper>
-          <TodoDetail key={todo.id}>
-            <TodoTitle>{todo.title}</TodoTitle>
-            <TodoDescription>{todo.description}</TodoDescription>
-          </TodoDetail>
-          <ButtonWrapper>
-            <Button>완료</Button>
-            <Button
-              onClick={() => handleDelete(todo.id)}
-              borderColor={colors.mainPink}
-            >
-              삭제하기
-            </Button>
-          </ButtonWrapper>
-        </TodoListWrapper>
-      ))}
+      <ProgressWrapper>
+        <WorkingWrapper>
+          <ProgressTitle>working🔥</ProgressTitle>
+          {props.todos
+            .filter((todo) => !todo.isDone)
+            .map((todo) => (
+              <TodoListWrapper>
+                <TodoDetail key={todo.id}>
+                  <TodoTitle>{todo.title}</TodoTitle>
+                  <TodoDescription>{todo.description}</TodoDescription>
+                </TodoDetail>
+                <ButtonWrapper>
+                  <Button onClick={() => handleProgressUpdate(todo.id)}>
+                    완료
+                  </Button>
+                  <Button
+                    onClick={() => handleDelete(todo.id)}
+                    borderColor={colors.mainPink}
+                  >
+                    삭제하기
+                  </Button>
+                </ButtonWrapper>
+              </TodoListWrapper>
+            ))}
+        </WorkingWrapper>
+        <DoneWrapper>
+          <ProgressTitle color={colors.mainPink}>Done✅ </ProgressTitle>
+          {props.todos
+            .filter((todo) => todo.isDone)
+            .map((todo) => (
+              <TodoListWrapper>
+                <TodoDetail key={todo.id}>
+                  <TodoTitle>{todo.title}</TodoTitle>
+                  <TodoDescription>{todo.description}</TodoDescription>
+                </TodoDetail>
+                <ButtonWrapper>
+                  <Button onClick={() => handleProgressUpdate(todo.id)}>
+                    취소
+                  </Button>
+                  <Button
+                    onClick={() => handleDelete(todo.id)}
+                    borderColor={colors.mainPink}
+                  >
+                    삭제하기
+                  </Button>
+                </ButtonWrapper>
+              </TodoListWrapper>
+            ))}
+        </DoneWrapper>
+      </ProgressWrapper>
     </Container>
   );
 };
@@ -39,8 +79,36 @@ export default Todos;
 const Container = styled.div`
   display: flex;
   justify-content: center;
-  flex-direction: column;
+  flex-direction: row;
   align-items: center;
+  gap: 40px;
+`;
+
+const ProgressWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 40px;
+`;
+const WorkingWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const ProgressTitle = styled.div<{ color?: string }>`
+  display: flex;
+  justify-content: center;
+  width: 400px;
+  height: 60px;
+  background-color: ${(props) => props.color || colors.mainYellow};
+  align-items: center;
+  font-size: 40px;
+  margin-bottom: 20px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+`;
+
+const DoneWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
 `;
 
 const TodoListWrapper = styled.div`
@@ -53,7 +121,7 @@ const TodoListWrapper = styled.div`
   border: 5px solid ${colors.mainColor};
 
   margin-bottom: 20px;
-  //filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 `;
 
 const TodoDetail = styled.div`
